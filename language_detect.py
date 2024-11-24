01d58674a725d04c929465fd3ba24860e8378411
Jacob pc <jacob.bore>
2024-11-24 15:05:24 +0100

    updated language detect

--- language_detect.py
+++ language_detect.py
@@ -1,23 +1,3 @@
-import sqlite3
-import pandas as pd
-def get_data_from_db(db_name = "beer_data.db"):
-    # Create a SQL connection to our SQLite database
-    con = sqlite3.connect(db_name)
-
-    cur = con.cursor()
-
-    #cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    #print(f"Table Name : {cur.fetchall()}")
-
-    df = pd.read_sql_query('SELECT * FROM beers', con)
-    con.close()
-    return df
-
-df = get_data_from_db()
-
-
-
-
 
 from langdetect import detect, DetectorFactory, LangDetectException
 
@@ -34,7 +14,6 @@ def safe_detect(text):
     except LangDetectException:
         return "unknown"
     
-
 
 
 
@@ -60,5 +39,3 @@ def detect_language(df):
 
 
 
-dff = detect_language(df)
-print(dff.head())
