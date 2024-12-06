332440d42cdad03b7d23606581f006a0b24acd26
lucasbrylle <lucasbrylle6@gmail.com>
2024-12-06 14:54:54 +0100

    Organized

---
+++ misc/get_data_from_db.py
@@ -0,0 +1,15 @@
+import sqlite3
+import pandas as pd
+
+def get_df(db_name = "beer_data.db"):
+    # Create a SQL connection to our SQLite database
+    con = sqlite3.connect(db_name)
+
+    cur = con.cursor()
+
+    #cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    #print(f"Table Name : {cur.fetchall()}")
+
+    df = pd.read_sql_query('SELECT * FROM beers', con)
+    con.close()
+    return df
